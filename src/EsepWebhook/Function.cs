cd03c9e75e7e1a347790938fd338f98b68528106
paulj003 <pauljablonowski@ufl.edu>
2023-11-13 20:13:18 -0500

    Modified Lambda function

---
+++ src/EsepWebhook/Function.cs
@@ -0,0 +1,41 @@
+using System.Text;
+using Amazon.Lambda.Core;
+using Newtonsoft.Json;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace EsepWebhook;
+
+public class Function
+{
+    
+    /// <summary>
+    /// A simple function that takes a string and does a ToUpper
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
+
+    // Used this website to learn about the environment variable file https://www.freecodecamp.org/news/what-are-environment-variables-and-how-can-i-use-them-with-gatsby-and-netlify/
+    // Used this website to learn about .gitignore https://www.freecodecamp.org/news/gitignore-what-is-it-and-how-to-add-to-repo/
+    // Copied the code from the example code provided
+
+    public string FunctionHandler(string input, ILambdaContext context)
+    {
+        dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
+        
+        string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
+        
+        var client = new HttpClient();
+        var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
+        {
+            Content = new StringContent(payload, Encoding.UTF8, "application/json")
+        };
+
+        var response = client.Send(webRequest);
+        using var reader = new StreamReader(response.Content.ReadAsStream());
+            
+        return reader.ReadToEnd();
+    }
+}
